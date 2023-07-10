d8622810e15ee143e334d7410035129d30409a31
Natália Sue <112422775+nataliasues@users.noreply.github.com>
2023-07-10 18:13:16 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,368 @@
+let questions = [
+  {
+  numb: 1,
+  question: "Pejorativo é algo que pode...",
+  answer: "ofender",
+  options: [
+    "alegrar",
+    "ofender",
+    "crescer",
+    "cair",
+    "agradar"
+  ]
+},
+  {
+  numb: 2,
+  question: 'Qual o tipo de sujeito classificado na frase "Ela é muito chata"?',
+  answer: "Simples",
+  options: [
+    "Oculto",
+    "Composto",
+    "Simples",
+    "Derivado",
+    "Inexistente"
+  ]
+},
+  {
+  numb: 3,
+  question: 'Qual o predicativo do sujeito na frase "O café é forte"?',
+  answer: "Forte",
+  options: [
+    "Forte",
+    "É",
+    "Café",
+    "Café forte",
+    "Nenhuma das alternativas"
+  ]
+},
+  {
+  numb: 4,
+  question: "Eu trabalho de segunda ____ sexta.",
+  answer: "a",
+  options: [
+    "a",
+    "à",
+    "ha",
+    "há",
+    "Nenhuma das alternativas"
+  ]
+},
+  {
+  numb: 5,
+  question: "___________ três meses que parei de estudar.",
+  answer: "Faz",
+  options: [
+    "Faz",
+    "Fazem",
+    "Faziam",
+    "Todas estão corretas"
+  ]
+},
+  {
+  numb: 6,
+  question: "Quais se classificam em artigos?",
+  answer: "Eu, tu, ele, nós, vós",
+  options: [
+    "De, em, desde, sobre",
+    "Eu, tu, ele, nós, vós",
+    "A, O, Um, Uns",
+    "Me, comigo, te, contigo",
+    "Nenhuma das alternativas"
+  ]
+  },
+  
+  {
+  numb: 7,
+  question: '"O tráfico no Rio de Janeiro é bastante preocupante". Na frase, o turista diz que:',
+  answer: "A cidade está muito perigosa, em relação ao crime organizado.",
+  options: [
+    "A cidade está muito perigosa, em relação ao crime organizado.",
+    "Os pedestres bloqueiam o caminho.",
+    "As motos aceleram muito.",
+    "Os carros estacionam em local proibido.",
+    "O lixo deixa a cidade suja."
+  ]
+  },
+  
+  {
+  numb: 8,
+  question: "Muitos alunos sentem dificuldade em saber o uso de ASSENTO ou ACENTO. Diferencie elas, respectivamente:",
+  answer: "Para sentar e sinal gráfico.",
+  options: [
+    "Sinal gráfico e móvel.",
+    "Casamento e sinal gráfico.",
+    "Para sentar e sinal gráfico.",
+    "Para sentar e móvel.",
+    "Nenhuma das alternativas."
+    ]
+  },
+  {
+  numb: 9,
+  question: 'Qual é a figura de linguagem presente na expressão "Ao vê-la bateu mais forte meu coração"',
+  answer: "Inversão",
+  options: [
+    "Hipérbole",
+    "Inversão",
+    "Antítese",
+    "Gradação",
+    "Comparação"
+  ]
+  },
+  {
+    numb: 10,
+  question: 'Qual é a figura de linguagem presente na expressão "Adoro quando as pessoas que gosto me ignoram..."',
+  answer: "Ironia",
+  options: [
+    "Ironia",
+    "Catacrese",
+    "Disfemismo",
+    "Anacoluto",
+    "Anáfora"
+  ]
+  },
+
+  {
+    numb: 11,
+    question: "Qual a fórmula para calcular a área de um triângulo?",
+    answer: "A = (b x h) / 2",
+    options: [
+      "A = I x I",
+      "A = b x h",
+      "A = (b x h) / 2",
+      "A = b x h / 2",
+      "Nenhuma das alternativas"
+    ]
+  },
+    {
+    numb: 12,
+    question: "Qual é o nome da linha que divide um círculo em duas partes iguais?",
+    answer: "Raio",
+    options: [
+      "Diagonal",
+      "Corda",
+      "Raio",
+      "Diâmetro",
+      "Nenhuma das alternativas"
+    ]
+  },
+    {
+    numb: 13,
+    question: "Qual é o resultado de 10 - 6 x 2?",
+    answer: "-2",
+    options: [
+      "2",
+      "-2",
+      "-12",
+      "12",
+      "8"
+    ]
+  },
+    {
+    numb: 14,
+    question: "Qual é o resultado de 25% de 80?",
+    answer: "20",
+    options: [
+      "15",
+      "20",
+      "25",
+      "23",
+      "12"
+    ]
+  },
+    {
+    numb: 15,
+    question: "Qual é o número que está faltando na sequência: 1, 4, 9, 16, ___?",
+    answer: "25",
+    options: [
+      "20",
+      "25",
+      "30",
+      "36",
+      "28"
+    ]
+  },
+    {
+    numb: 16,
+    question: "Qual é o resultado de 5² + 7²?",
+    answer: "74",
+    options: [
+      "60",
+      "74",
+      "84",
+      "96",
+      "Nenhuma das alternativas"
+    ]
+    },
+    
+    {
+    numb: 17,
+    question: "Qual é a fórmula para calcular a área de um retângulo?",
+    answer: "A = b x h",
+    options: [
+      "A = π x r²",
+      "A = b x h",
+      "A = b + h",
+      "A = 2 x (b + h)",
+      "Nenhuma das alternativas"
+    ]
+    },
+    
+    {
+    numb: 18,
+    question: "(FAETEC - 2015) Um pacote do biscoito Saboroso custa R$ 1,25. Se João comprou N pacotes desse biscoito gastando R$ 13,75, o valor de N é igual a:",
+    answer: "11",
+    options: [
+      "11",
+      "12",
+      "13",
+      "14",
+      "15"
+      ]
+    },
+    {
+    numb: 19,
+    question: "(IF-BA) A hipotenusa do triângulo retângulo de catetos (4 + √5) cm e (4 - √5) cm é igual ao lado de um quadrado de área x cm². Determine o valor de x.",
+    answer: "42",
+    options: [
+      "34",
+      "20",
+      "81",
+      "42",
+      "54"
+    ]
+    },
+    {
+      numb: 20,
+    question: "Qual das proporções abaixo são iguais à razão entre 4 e 6?",
+    answer: "2 e 3",
+    options: [
+      "2 e 3",
+      "2 e 4",
+      "4 e 12",
+      "4 e 8",
+      "Nenhuma das alternativas"
+    ]
+    },
+    {
+      numb: 21,
+    question: "Sobre a recombinação gênica é correto afirmar que: ",
+    answer: "A recombinação gênica é classificada em homóloga e não-homóloga",
+    options: [
+      "A recombinação gênica pode ser homóloga ou heteróloga.",
+      "A principal diferença entre os tipos de recombinação é que ocorrem em etapas diferentes da mitose.",
+      "A recombinação gênica é classificada em homóloga e não-homóloga.",
+      "Enquanto um tipo de recombinação permite a variabilidade, o outro tipo apresenta limitações.",
+      "Acontece apenas em séries homólogas após a mitose."
+    ]
+    },
+    {
+      numb: 22,
+    question: "(Fatec) Pares de genes, com segregação independente podem agir, conjuntamente, na determinação de uma mesma característica fenotípica. Este fenômeno é conhecido como:",
+    answer: "interação gênica",
+    options: [
+      "interação gênica",
+      "epistasia",
+      "poligenia",
+      "dominância completa",
+      "herança qualitativa"
+    ]
+    },
+    {
+      numb: 23,
+    question: "A mutação genética difere da recombinação gênica, pois: ",
+    answer: "enquanto a mutação corresponde a uma mudança hereditária no DNA, a recombinação se dá pela mistura de genes com seres da mesma espécie.",
+    options: [
+      "enquanto a mutação corresponde a uma mudança hereditária no DNA, a recombinação cria novos genes.",
+      "enquanto a mutação corresponde a uma mudança hereditária no DNA, a recombinação se dá pela mistura de genes com seres da mesma espécie.",
+      "A mutação é espontânea e a recombinação é induzida.",
+      "A mutação é uma fonte secundária de variação, enquanto a recombinação é uma fonte primária.",
+      "A mutação ocorre, obrigatóriamente, em conjunto com a recombinação gênica."
+    ]
+    },
+    {
+      numb: 24,
+    question: "Das alternativas a seguir, qual NÃO apresenta um tipo de seleção natural",
+    answer: "Seleção dimensional",
+    options: [
+      "Seleção direcional",
+      "Seleção estabilizadora",
+      "Seleção disruptiva",
+      "Seleção dimensional",
+      "Nenhuma das alternativas"
+    ]
+    },
+    {
+      numb: 25,
+    question: "(PUC - RIO) Uma característica genética recessiva presente no cromossomo Y é: ",
+    answer: "só poder ser herdada a partir do pai por seus descendentes do sexo masculino.",
+    options: [
+      "poder ser herdada do pai ou da mãe pelos descendentes do sexo masculino e do feminino.",
+      "só poder ser herdada a partir do pai por seus descendentes do sexo masculino.",
+      "só poder ser herdada a partir do pai por seus descendentes do sexo feminino.",
+      "só poder ser herdada a partir da mãe por seus descendentes do sexo masculino.",
+      "só poder ser herdada a partir da mãe por seus descendentes do sexo feminino."
+    ]
+    },
+    {
+      numb: 26,
+    question: "(UFPB) Os componentes celulares que estão presentes tanto em células de eucariontes como de procariontes são:",
+    answer: "membrana plasmática e ribossomos.",
+    options: [
+      "membrana plasmática e mitocôndrias.",
+      "mitocôndrias e ribossomos.",
+      "ribossomos e lisossomos.",
+      "lisossomos e membrana plasmática.",
+      "membrana plasmática e ribossomos."
+    ]
+    },
+    {
+      numb: 27,
+    question: "(Fuvest-SP) Entre as características de uma célula vegetal, que a distingue de uma célula animal, podem ser citadas a presença de:",
+    answer: "parede celular e a ocorrência da fotossíntese, processo que depende de água, gás carbônico e luz.",
+    options: [
+      "cloroplastos e a ocorrência da fotossíntese, processo que depende de gás carbônico, oxigênio e luz.",
+      "parede celular e a ocorrência da fotossíntese, processo que depende de água, gás carbônico e luz.",
+      "mitocôndrias e a ocorrência da fotossíntese, processo que depende de água, gás carbônico e oxigênio.",
+      "cloroplastos e a ocorrência da respiração celular, processo que depende de água, gás carbônico e nitrogênio.",
+      "parede celular e a ocorrência da respiração celular, processo que depende de água, oxigênio e luz."
+    ]
+    },
+    {
+      numb: 28,
+    question: "O nome do hormônio responsável por estimular a produção do leite durante a amamentação é: ",
+    answer: "prolactina",
+    options: [
+      "progesterona",
+      "hipófise",
+      "testosterona",
+      "estrogênio",
+      "prolactina"
+    ]
+    },
+    {
+      numb: 29,
+    question: "A ______ é o hormônio produzidos pelos ______ sendo responsável pelo desenvolvimento das características sexuais _________.",
+    answer: "prolactina",
+    options: [
+      "prolactina; canais deferentes; femininas",
+      "adrenalina; epidídimos; femininas",
+      "testosterona; testículos; masculinas",
+      "progesterona; canais deferentes; masculinas",
+      "hipófise; testículos; masculinas"
+    ]
+    },
+    {
+      numb: 30,
+    question: "Sobre o sistema genital masculino, assinale a alternativa INCORRETA",
+    answer: "a formação dos espermatozoides é denominado de gametogênese.",
+    options: [
+      "o esperma ou o sêmen é composto por espermatozoides.",
+      "nos homens, a uretra é um canal que serve ao sistema urinário e ao sistema reprodutor.",
+      "através do pênis são eliminados a urina e o sêmen.",
+      "o líquido seminal, produzido na vesícula seminal, ajuda a neutralizar a acidez da vagina durante a relação sexual.",
+      "a formação dos espermatozoides é denominado de gametogênese."
+    ]
+    },
+
+]
+
